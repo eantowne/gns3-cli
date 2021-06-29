1c9a46a4a46c3315a5271b17005f57f7a4da24a6
eantowne <66289315+eantowne@users.noreply.github.com>
2021-06-29 14:42:12 -0500

    Most feature completion update
    Added:
    * Display Looping
    * CLI agruments

--- gns3-ts.py
+++ gns3-ts.py
@@ -1,12 +1,28 @@
-from sys import exit
+import os
+import sys
+import time
 import requests
 import json
+import argparse
+import threading as th
 
-ip = 'localhost'    # IP address/hostname/fqdn of server that is controller
-port = "3080"       # Release VM's use 80, local install, and install via PIP/APT use 3080
+if os.name == 'nt':
+
+    import msvcrt
+    import ctypes
+
+    class _CursorInfo(ctypes.Structure):
+        _fields_ = [("size", ctypes.c_int),
+                    ("visible", ctypes.c_byte)]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('address', type=str, help='ip/host Name/FQDN the controller is running on')
+parser.add_argument('port', type=str, help='port number the controller is running on', )
+parser.add_argument('interval', type=int, help='refresh interval in seconds')
+args = parser.parse_args()
 protocol_type = 'HTTP'      # If using HTTPS, change to True
 
-url = f'{protocol_type}://{ip}:{port}/v2/'
+url = f'{protocol_type}://{args.address}:{args.port}/v2/'
 
 # Define colors
 c_red = '\33[31m'
@@ -19,6 +35,43 @@ b = ' | STATUS:'
 c = '| CONSOLE'
 d = '=========================================='
 
+def hide_cursor():
+
+    if os.name == 'nt':
+        ci = _CursorInfo()
+        handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+        ci.visible = False
+        ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+    elif os.name == 'posix':
+        sys.stdout.write("\033[?25l")
+        sys.stdout.flush()
+
+def show_cursor():
+
+    if os.name == 'nt':
+        ci = _CursorInfo()
+        handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+        ci.visible = True
+        ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+    elif os.name == 'posix':
+        sys.stdout.write("\033[?25h")
+        sys.stdout.flush()
+
+def get_screen_clear_cmd():
+
+    if os.name == 'posix': 
+        return 'clear'
+    else:
+        return 'cls'
+
+def key_capture_thread():
+
+    global keep_going
+    input()
+    keep_going = False
+
 project_id = ''
 project_name = ''
 
@@ -27,18 +80,14 @@ def get_json_resp(api_call):
     r = ''
 
     try:
-
         r = requests.get(url + api_call)
-
         if r.status_code == 200:
-
             # Convert response from controller into JSON Dict
-            return json.loads(json.dumps(r.json()))
-        
-        else: print(f'Invalid response code "{r.status_code}" from controller at: {url}')
+            return json.loads(json.dumps(r.json()))        
+        else: exit(f'Invalid response code "{r.status_code}" from controller at: {url}')
 
     except:
-        exit('Error: Failed most likely due to not being able to establish a connection')
+        exit('\nError: Failed most likely due to not being able to establish a connection')
 
 def get_open_project():
 
@@ -46,14 +95,14 @@ def get_open_project():
     for project in range(len(resp)):
 
         #print(resp[project]['status'])
-        
+        status = 'closed'
         if resp[project]['status'] == 'opened':
            
             return resp[project]['project_id'], resp[project]['name']
 
-        else:
+    if status == 'closed': return 'closed'
 
-            return 'closed'
+
 
 def print_nodes(uuid):
 
@@ -80,15 +129,38 @@ def print_nodes(uuid):
 
         print(f'{name:25} | {status:15} | {console_type:15} {console_port}')
 
-if get_open_project() == 'closed':
-    
-    exit('\nNo open project, please open one and retry.')
-    
-else:
 
-    project = get_open_project()
-    project_id = project[0]
-    project_name = project[1]
-    print(f'\nCurrently open project: {c_green + project[1] + c_default:15}\nUUID: {project[0]}\n')
+clear_cmd = get_screen_clear_cmd()
+keep_going = True
+hide_cursor()
+os.system(clear_cmd)
+th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
 
-print_nodes(project_id)
+while keep_going:
+
+    try:
+
+        if get_open_project() == 'closed':
+    
+            print('\nNo open project, please open one and retry.')
+            break
+        
+        else:
+
+            project = get_open_project()
+            project_id = project[0]
+            project_name = project[1]
+            print(f'\nCurrently open project: {c_green + project[1] + c_default:15}\n')
+            print_nodes(project_id)
+            print('\nPress [ENTER] to quit')
+            time.sleep(args.interval)
+            os.system(clear_cmd)
+
+    except:
+
+        print(f'\nError: Failed to connect to server: {args.address}:{args.port} most likely due to not being able to establish a connection')
+        show_cursor()
+        break
+
+print('Exiting')
+show_cursor()
