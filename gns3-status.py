4251e6fd264eec649f50bbd8635cffaff6513b01
eantowne <66289315+eantowne@users.noreply.github.com>
2021-06-29 08:52:51 -0500

    Started refactor to include functions for seperating common reusable code based on work from gns3-ts.py

--- gns3-status.py
+++ gns3-status.py
@@ -1,3 +1,5 @@
+import os
+import time
 import requests
 import json
 
@@ -5,7 +7,7 @@ ip = 'localhost'    # IP address/hostname/fqdn of server that is controller
 port = "3080"       # Release VM's use 80, local install, and install via PIP/APT use 3080
 protocol_type = 'HTTP'      # If using HTTPS, change to True
 
-url = f'{protocol_type}://{ip}:{port}/v2/computes'
+url = f'{protocol_type}://{ip}:{port}/v2/'
 
 # Define colors
 c_red = '\33[31m'
@@ -19,33 +21,39 @@ c = ' | CPU:'
 d = ' | RAM:'
 e = '=========================================='
 
-# send GET request for list of computes to controller
+def get_json_resp(api_call):
 
-r = ''
+    r = ''
+
+    try:
+
+        r = requests.get(url + api_call)
+
+        if r.status_code == 200:
+
+            # Convert response from controller into JSON Dict
+            return json.loads(json.dumps(r.json()))
+        
+        else: print(f'Invalid response code "{r.status_code}" from controller at: {url}')
+
+    except:
+        exit('\nError: Failed most likely due to not being able to establish a connection')
 
 try:
-    r = requests.get(url)
+    resp = get_json_resp('computes')
 
-    if r.status_code == 200:
+    # Print header row
+    print(f'\n{a:15}{b:10}{c:8}{d:8}\n{e}')
 
-        # Convert response from controller into JSON Dict
-        computes = json.loads(json.dumps(r.json()))
-
-        # Print header row
-        print(f'\n{a:15}{b:10}{c:8}{d:8}\n{e}')
-
-        # Extract, format and print to console applicable data for status
-        for compute in range(len(computes)):
-            host = computes[compute]['name']
-            if computes[compute]['connected']:
-                status = c_green + 'UP' + c_default
-            else: status = c_red + 'DOWN' + c_default
-            cpu = computes[compute]['cpu_usage_percent']
-            ram = computes[compute]['memory_usage_percent']
-            print(f'{host:15} | {status:16} | {cpu:4}% | {ram:4}%')
-
-    else: print(f'Invalid response code "{r.status_code}" from controller at: {url}')
+    # Extract, format and print to console applicable data for status
+    for compute in range(len(resp)):
+        host = resp[compute]['name']
+        if resp[compute]['connected']:
+            status = c_green + 'UP' + c_default
+        else: status = c_red + 'DOWN' + c_default
+        cpu = resp[compute]['cpu_usage_percent']
+        ram = resp[compute]['memory_usage_percent']
+        print(f'{host:15} | {status:16} | {cpu:4}% | {ram:4}%')
 
 except:
-    print('Error: Failed most likely due to not being able to establish a connection')
-
+    print('\nError: Failed most likely due to not being able to establish a connection')
